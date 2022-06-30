98efa2d03a257181e4e9d13f05f6898e347ac390
yenminyoung <87947236+yenminyoung@users.noreply.github.com>
2022-06-30 13:57:04 +0000

    added 1 folder

---
+++ programming/1/Nim.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Nim{
+  public static void main(String[] args) {
+    int stones = 12;
+    int stonesTaken;
+
+    Scanner iput = new Scanner(System.in);
+
+    // loop until game ends
+    while(){
+      // prompt user for input: user's turn  
+
+      // calculate # of stones remaining and print
+
+      // check for win
+
+      // machine turn
+
+      // calculate # of stones remaining and print
+
+      // check for win
+      
+    }
+  }
+  
+}
